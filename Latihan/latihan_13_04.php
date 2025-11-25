7730a9d072e4d0bcb91f20a5fabecd1103974640
ghaniya <ghaniyagst@gmail.com>
2025-11-25 19:23:01 +0700

    hapus

--- Latihan/latihan_13_04.php
+++
@@ -1,9 +0,0 @@
-<?php
-abstract class Mobil {
-    abstract public function lihat_spesifikasi($nama_pemilik);
-    public static function hidupkan_mobil(){
-        echo "Hidupkan Mobil";
-    }
-}
-echo Mobil::hidupkan_mobil();
-?>
